70379833d741ed6a4d057e93d3925975fb4e9449
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2023-04-23 15:58:59 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,8 @@
 rconPort="9100"
 serverIp="127.0.0.1"
 rconPassWord="PASSWORD123"
-#not md5 or if u want to use md5 just comment line #8 out with a # at the start and remove the leading # on line 9
+#not md5 or if u want to use md5 just comment line #9 out with a # at the start and
+remove the leading # on line 10
 
 HASH=$(echo -n "${rconPassWord}" | md5sum)
 #HASH="MD5 PASSWORD"
