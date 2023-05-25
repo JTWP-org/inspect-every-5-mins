40ad9534fa93e15c98fd4d0c42e3899b769f156e
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2023-05-25 19:01:34 -0400

    Update nc.sh

--- nc.sh
+++ nc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#script here was not made by me the slowcat was written by invicta_nz#6872 and found in OLD wiki 
+
 source config.sh
  
 function slowcat(){ while read; do sleep .05; echo "$REPLY"; done; }
 cat  $1 | slowcat | nc ${serverIp} ${rconPort}
+
+
